14aff75ce599f45267f614d745eab689ba1bcce1
nmorelli96 <morellinicolas96@gmail.com>
2022-06-20 19:08:04 -0300

    Add navbar, footer, styles for all the app, and changes in textarea placeholder

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 marked.setOptions({
-  gfm: false,
+  gfm: true,
   breaks: true
 })
 
@@ -21,43 +21,53 @@ class App extends React.Component {
     const { text } = this.state;
     const markdown = marked.parse(text);
     return (
-      <div className="container-fluid">
-        {/* Tabs */}
-        <ul className="nav nav-tabs d-flex d-md-none" role="tablist">
-          <li className="nav-item"><a className="nav-link active" href="#editor-pane" aria-controls="editor"
-            data-bs-toggle="tab">Editor</a></li>
-          <li className="nav-item"><a className="nav-link" href="#markdown-pane" aria-controls="profile"
-            data-bs-toggle="tab">Markdown</a></li>
+      <div className="container-fluid p-0">
+        <nav class="navbar navbar-light top d-flex align-content-center justify-content-center" id="navbar">
+          <p class="navbar-brand h1 m-0 p-0 unselectable"><span>&lt; </span>Markdown Previewer <span>/&gt;</span></p>
+        </nav>
+        {/* Tabs only visible on mobile*/}
+        <ul className="nav nav-tabs d-flex d-xl-none align-content-center justify-content-center" role="tablist">
+          <li className="nav-item"><a className="nav-link active unselectable" href="#editor-pane" aria-controls="editor"
+            data-bs-toggle="tab"><i class="fa-solid fa-pen-to-square fa-sm"></i> Editor</a></li>
+          <li className="nav-item"><a className="nav-link unselectable" href="#markdown-pane" aria-controls="profile"
+            data-bs-toggle="tab"><i class="fa-solid fa-code fa-sm"></i> Markdown</a></li>
         </ul>
 
-        {/* Tabs replacement titles for big screen */}
-        <div className="title-container d-none d-md-flex justify-content-evenly">
-          <div className="content-title">
-            Editor
-          </div>
+        {/* Tabs replacement titles for desktop */}
+        <div className="title-container d-none d-xl-block">
+          <div className="row text-center h5">
+            <div className="content-title col p-0 unselectable">
+              <i class="fa-solid fa-pen-to-square fa-sm"></i> Editor
+            </div>
 
-          <div className="content-title">
-            Markdown
+            <div className="content-title col p-0 unselectable">
+              <i class="fa-solid fa-code fa-sm"></i> Markdown
+            </div>
           </div>
         </div>
 
         {/* Tabs content */}
-        <div className="tab-content row">
-          <div role="tabpanel" className="tab-pane active col col-12 col-md-6" id="editor-pane">
-            <textarea className="form-control" id="editor" onChange={this.handleChange} value={text}># Welcome to my React Markdown Previewer!
+        <div className="tab-content row align-content-center justify-content-center">
+          <div role="tabpanel" className="tab-pane active col col-12 col-xl-6" id="editor-pane">
+            <textarea className="form-control" id="editor" onChange={this.handleChange}
+              value={text}># Welcome to my React Markdown Previewer!
             </textarea>
           </div>
-          <div role="tabpanel" className="tab-pane col-12 col-md-6 " id="markdown-pane">
-            <div className="preview rounded" id="preview"
+          <div role="tabpanel" className="tab-pane col-12 col-xl-6" id="markdown-pane">
+            <div className="preview" id="preview"
               dangerouslySetInnerHTML={{ __html: markdown }} />
           </div>
         </div>
-      </div>
+        <footer className="footer text-muted d-flex align-content-center justify-content-center">Based on the&nbsp;<a target="_blank"
+          href="https://www.freecodecamp.org/learn/front-end-development-libraries/">
+          FCC course</a>&nbsp;by&nbsp; <a target="_blank"
+            href="https://github.com/nmorelli96/fcc-markdown-previewer">nmorelli96</a></footer>
+      </div >
     );
   }
 }
 
-const placeholder = `# Welcome to my React Markdown Previewer!
+const placeholder = `# Welcome!
 
 ## This is a sub-heading...
 ### And here's some other cool stuff:
